994be8cffa468001bd460a5298211131bdb0e770
a17joaap <a17joaap@student.his.se>
2020-04-05 03:48:03 +0200

    Batch send 100 objects instead of 1 at a time

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 const http = require('http');
 const WebsocketServer = require('websocket').server;
+const fs = require('fs');
 
+const raw = fs.readFileSync(__dirname + '/gpsdata2.json');
+const objects = JSON.parse(raw).points;
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-
-});
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+const server = http.createServer((req, res) => { });
 
 wsServer = new WebsocketServer({
     httpServer: server
@@ -20,9 +17,17 @@ wsServer = new WebsocketServer({
 wsServer.on('request', (req) => {
     const connection = req.accept(null, req.origin);
 
-
-    connection.on('message', (message) => {
-        //Start sending JSON
-        console.log(message);
+    connection.on('message', (_) => {
+        connection.send('Start');
+        while (objects.length > 0) {
+            toSend = objects.splice(0, 100);
+            connection.send(JSON.stringify(toSend));
+        }
+        connection.send('End');
     })
 })
+
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
