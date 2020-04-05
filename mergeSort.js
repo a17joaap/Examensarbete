453296b51e0a6c5d4cfbbb3b2aa3a6367376a7f8
a17joaap <a17joaap@student.his.se>
2020-04-05 03:45:58 +0200

    Added merge sort

---
+++ mergeSort.js
@@ -0,0 +1,33 @@
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const mid = Math.floor(arr.length / 2);
+    const left = arr.slice(0, mid);
+    const right = arr.slice(mid);
+
+    return merge(
+        mergeSort(left), mergeSort(right)
+    );
+}
+
+function merge(left, right) {
+    let res = [];
+    let leftIdx = 0;
+    let rightIdx = 0;
+
+    while (leftIdx < left.length && rightIdx < right.length) {
+        const firstDistance = getDistanceFromLatLonInKm(left[leftIdx].lat, left[leftIdx].lon);
+        const secondDistance = getDistanceFromLatLonInKm(right[rightIdx].lat, right[rightIdx].lon);
+        if (firstDistance < secondDistance) {
+            res.push(left[leftIdx]);
+            leftIdx++;
+        } else {
+            res.push(right[rightIdx]);
+            rightIdx++;
+        }
+    }
+
+    return res.concat(left.slice(leftIdx)).concat(right.slice(rightIdx));
+}
