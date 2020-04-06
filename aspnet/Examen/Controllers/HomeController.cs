18e52037d9ac8b5e09580b82c2c81b24d0360457
a17joaap <a17joaap@student.his.se>
2020-04-06 04:23:15 +0200

    AJAX response added

---
+++ aspnet/Examen/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Examen.Models;
+using Newtonsoft.Json;
+
+namespace Examen.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public string GetResult()
+        {
+            while (Program.finishedSorting == false)
+            {
+
+            }
+            return JsonConvert.SerializeObject(Program.result);
+        }
+    }
+}
