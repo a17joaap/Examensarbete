b56b2a41ba47cf463cbe239134a7363b810b900f
a17joaap <a17joaap@student.his.se>
2020-04-07 04:53:42 +0200

    Added variable for amount of records

--- timeMeasure.user.js
+++ timeMeasure.user.js
@@ -8,56 +8,57 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+  "use strict";
 
-    const container = document.querySelector('#animationContainer');
-    const button = document.querySelector('#get-stream');
-    const clickEvent = new MouseEvent('click');
-    let startTime, endTime, result;
-    let times = [];
+  const container = document.querySelector("#animationContainer");
+  const button = document.querySelector("#get-stream");
+  const clickEvent = new MouseEvent("click");
+  let startTime, endTime, result;
+  let times = [];
+  const TIMES_TO_RECORD = 10;
 
-    function respondToVisibility(element, callback) {
-        var options = {
-            root: document.documentElement
+  function respondToVisibility(element, callback) {
+    var options = {
+      root: document.documentElement,
+    };
+
+    var observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        callback(entry.intersectionRatio > 0);
+      });
+    }, options);
+
+    observer.observe(element);
+  }
+
+  respondToVisibility(container, (visible) => {
+    if (visible) {
+      startTime = new Date().getTime();
+    } else {
+      if (startTime) {
+        endTime = new Date().getTime();
+        result = endTime - startTime;
+        times.push(result);
+        if (times.length == TIMES_TO_RECORD) {
+          writeToCSV();
+          return;
         }
-
-        var observer = new IntersectionObserver((entries, observer) => {
-            entries.forEach(entry => {
-                callback(entry.intersectionRatio > 0);
-            });
-        }, options);
-
-        observer.observe(element);
+        button.dispatchEvent(clickEvent);
+      }
     }
+  });
 
-    respondToVisibility(container, visible => {
-        if (visible) {
-            startTime = new Date().getTime();
-        } else {
-            if (startTime) {
-                endTime = new Date().getTime();
-                result = endTime - startTime;
-                times.push(result);
-                if (times.length == 10) {
-                    writeToCSV();
-                    return;
-                }
-                button.dispatchEvent(clickEvent);
-            }
-        }
-    })
-
-    function writeToCSV() {
-        let csv = "data:text/csv;charset=utf-8, ";
-        times.forEach((entry) => {
-            csv += entry.toString()+"\n";
-        })
-        let a = document.createElement('a');
-        a.setAttribute('href', encodeURI(csv));
-        a.setAttribute('download', 'sortTimes.csv');
-        document.body.appendChild(a);
-        times = [];
-        a.click();
-    }
+  function writeToCSV() {
+    let csv = "data:text/csv;charset=utf-8, ";
+    times.forEach((entry) => {
+      csv += entry.toString() + "\n";
+    });
+    let a = document.createElement("a");
+    a.setAttribute("href", encodeURI(csv));
+    a.setAttribute("download", "sortTimes.csv");
+    document.body.appendChild(a);
+    times = [];
+    a.click();
+  }
 })();
